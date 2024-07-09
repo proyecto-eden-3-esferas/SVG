d72b14cc0fb402229e22d68893bf2d58a53d43fb
Francisco Fernández-Victorio Hernández <franciscofvh@hotmail.com>
2024-07-09 08:41:17 +0200

    transistor

--- Makefile
+++ Makefile
@@ -38,7 +38,7 @@ schematics.sq_polyline-and-block: schematics.sq_polyline-and-block.test.cpp sche
 %.o: %.cpp
 	g++ -c -std=c++17 $<
 
-EXECUTABLES = schematics.rectangle schematics.line schematics.block schematics.round schematics.ic schematics.svg.arc schematics.round-and-arrow segment schematics.label schematics.labeled_block schematics.sq_polyline-and-block schematics.twoport
+EXECUTABLES = schematics.rectangle schematics.line schematics.block schematics.round schematics.ic schematics.svg.arc schematics.round-and-arrow segment schematics.label schematics.labeled_block schematics.sq_polyline-and-block schematics.twoport schematics.transistor
 
 clean_executables:
 	$(RM) $(EXECUTABLES)
--- schematics.transistor.h
+++ schematics.transistor.h
@@ -25,31 +25,39 @@ public:
   typedef angle_addressable<FLOAT> cir_t;
   typedef FLOAT float_t;
   typedef std::size_t size_t;
-  enum points {emitter, base, collector, emitter_side_start_of_base, collector_side_start_of_base, from0, from1, from2};
+  enum points {emitter, base, collector=2,
+               emitter_side_start_of_base=3, collector_side_start_of_base=4,
+               emitter_meets_base=5, collector_meets_base=6,
+               from0, from1, from2};
   using circular_t::cx, circular_t::cy, circular_t::r;
   using circular_t::xperim, circular_t::yperim;
-  FLOAT ae;    // angle of the emitter
-  FLOAT ac;    // angle of the collector
-  FLOAT abase; // angle of the base
-  FLOAT kbase; // k is a factor: k==1.0 means the base is drawn as a chord
+  FLOAT ae;    // angle of the emitter   to the X axis
+  FLOAT ac;    // angle of the collector to the X axis
+  FLOAT abase; // angle of the base      to the X axis
+  FLOAT kbase;  // k is a factor: k==1.0 means the base is drawn as a chord
+  FLOAT kkbase; // k==1.0 means emitter and collector leads meat the base at the edge
   FLOAT x_of_pin(size_t idx) const;
   FLOAT y_of_pin(size_t idx) const;
   template <typename OUT = std::ostream>
   OUT& add_circle_to_svg(OUT& o) const;
+  // Lines inside the transistor circle: base, emitter, collector
   template <typename OUT = std::ostream>
   OUT& add_base_to_svg(OUT& o) const;
   template <typename OUT = std::ostream>
   OUT& add_emitter_to_svg(OUT& o) const;
   template <typename OUT = std::ostream>
+  OUT& add_collector_to_svg(OUT& o) const;
+  template <typename OUT = std::ostream>
   OUT& add_svg(OUT& o) const;
   //
   transistor(float_t ra, float_t x=0, float_t y=0,
-             float_t ab = cir_t::deg_to_rad(120),
+             float_t ab = cir_t::deg_to_rad(130),
              float_t ap = cir_t::deg_to_rad(60),
-             float_t kb = 0.2) :
-    circular_t(ra,x,y), ae(ap), ac(ap), abase(ab), kbase(0.2) {};
+             float_t kb = 0.8) :
+    circular_t(ra,x,y), ae(ap), ac(ap), abase(ab), kbase(kb), kkbase(kb) {};
 };
-//
+/* Implementations of transistor<FLOAT> methods
+ */
 template <typename FLOAT>
 template <typename OUT>
 OUT& transistor<FLOAT>::add_circle_to_svg(OUT& o) const {
@@ -67,13 +75,21 @@ template <typename FLOAT>
 template <typename OUT>
 OUT& transistor<FLOAT>::add_emitter_to_svg(OUT& o) const {
   o << "<line x1=\"" << x_of_pin(0) << "\" y1=\"" << y_of_pin(0) << '\"';
-  o <<      " x2=\"" << x_of_pin(3) << "\" y2=\"" << y_of_pin(3) << "\"/>\n";
+  o <<      " x2=\"" << x_of_pin(5) << "\" y2=\"" << y_of_pin(5) << "\"/>\n";
+  return o;
+};
+template <typename FLOAT>
+template <typename OUT>
+OUT& transistor<FLOAT>::add_collector_to_svg(OUT& o) const {
+  o << "<line x1=\"" << x_of_pin(2) << "\" y1=\"" << y_of_pin(2) << '\"';
+  o <<      " x2=\"" << x_of_pin(6) << "\" y2=\"" << y_of_pin(6) << "\"/>\n";
   return o;
 };
 template <typename FLOAT>
 template <typename OUT>
 OUT& transistor<FLOAT>::add_svg(OUT& o) const {
   add_emitter_to_svg(o);
+  add_collector_to_svg(o);
   add_circle_to_svg(o);
   return add_base_to_svg(o);
 };
@@ -81,30 +97,29 @@ OUT& transistor<FLOAT>::add_svg(OUT& o) const {
 template <typename FLOAT>
 FLOAT transistor<FLOAT>::x_of_pin(size_t idx) const {
   switch (idx) {
-    case 0: return circular_t::cx + circular_t::r * cir_t::cos(ae);
-    case 1: return circular_t::cx - circular_t::r;
-    case 2: return circular_t::cx + circular_t::r * cir_t::cos(ac);
-    case 3: return circular_t::cx + circular_t::r * cir_t::cos(abase);
-    case 4: return circular_t::cx + circular_t::r * cir_t::cos(abase);
+    case 0: return cx + r * cir_t::cos(ae);
+    case 1: return cx - r;
+    case 2: return cx + r * cir_t::cos(ac);
+    case 3:// return cx + r * cir_t::cos(abase);
+    case 4:// return cx + r * cir_t::cos(abase);
     // Inside the transistor circle, along the base, top to bottom:
     case 5:
-    case 6:
-    case 7: return circular_t::cx + circular_t::r * cir_t::cos(ac);;
+    case 6: return cx + r * cir_t::cos(abase);
+    case 7: return cx + r * cir_t::cos(ac);;
     default: return 0.0;
   }
 };
 template <typename FLOAT>
 FLOAT transistor<FLOAT>::y_of_pin(size_t idx) const {
   switch (idx) {
-    case 0: return circular_t::cy + circular_t::r * cir_t::sin(ae);
-    case 1: return circular_t::cy - circular_t::r;
-    case 2: return circular_t::cy + circular_t::r * cir_t::sin(ac);
-    case 3: return circular_t::cy - circular_t::r * cir_t::sin(abase);
-    case 4: return circular_t::cy + circular_t::r * cir_t::sin(abase);
+    case 0: return cy - r * cir_t::sin(ae);
+    case 1: return cy - r;
+    case 2: return cy + r * cir_t::sin(ac);
+    case 3: return cy - kbase * r * cir_t::sin(abase);
+    case 4: return cy + kbase * r * cir_t::sin(abase);
     // Inside the transistor circle, along the base, top to bottom:
-    case 5: return circular_t::cy - circular_t::r * cir_t::sin(abase);
-    case 6: return circular_t::cy ;
-    case 7: return circular_t::cy + circular_t::r * cir_t::sin(abase);
+    case 5: return cy - kkbase * kbase * r * cir_t::sin(abase);
+    case 6: return cy + kkbase * kbase * r * cir_t::sin(abase);
     default: return 0.0;
   }
 };
